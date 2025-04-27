22419d46dc6bf52f06d1bfa637795b74bea2d98d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-04-26 21:55:33 -0600

    delete: remove none-ls and none-ls-extras plugins

--- nvim/.config/nvim/lua/plugins/none-ls-extras.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"nvimtools/none-ls-extras.nvim",
-	dependencies = {
-		"nvimtools/none-ls.nvim",
-	},
-}
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvimtools/none-ls.nvim",
-	config = function(lazyplugin, opts)
-		local null_ls = require("null-ls")
-		null_ls.setup(require("plugins.opts.none-ls").build_opts(null_ls))
-		require("plugins.config.none-ls")
-	end,
-	enabled = false,
-}
