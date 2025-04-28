be45a500233d7d4cde440774a0de0630f69f771a
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-04-28 15:45:23 -0600

    add: vimux strategy for vim-test

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -3,6 +3,11 @@ local config = require("config")
 
 return {
 	"vim-test/vim-test",
-	opts = require("plugins.opts.vim-test"),
+	dependencies = {
+		"preservim/vimux",
+	},
 	keys = util.map_array(config.mapping.get_namespaced_keymaps("vim-test"), util.keymap_to_lazykey),
+	config = function(lazyplugin, opts)
+		require("plugins.config.vim-test")
+	end,
 }
