be06e2304b2a48edf5602caad57ea35c2797cc7d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-05-07 15:50:15 -0600

    chore: formatting

--- nvim/.config/nvim/lua/plugins/config/nvim-lspconfig/helpers/set_common_keymaps.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-lspconfig/helpers/set_common_keymaps.lua
@@ -8,27 +8,25 @@ local M = {}
 ---@param keymap config.mapping.KeyMap
 ---@param extra_opts? config.mapping.ExtraKeymapsetOpts
 local set_keymap = function(keymap, extra_opts)
-  ---@type vim.keymap.set.Opts
-  local opts = {}
-  opts.desc = keymap.opts.desc
-  opts.noremap = keymap.opts.noremap
-  if extra_opts ~= nil then
-    if extra_opts.buffer ~= nil then
-      opts.buffer = extra_opts.buffer
-    end
-  end
+	---@type vim.keymap.set.Opts
+	local opts = {}
+	opts.desc = keymap.opts.desc
+	opts.noremap = keymap.opts.noremap
+	if extra_opts ~= nil then
+		if extra_opts.buffer ~= nil then
+			opts.buffer = extra_opts.buffer
+		end
+	end
 
-  vim.keymap.set(keymap.mode, keymap.lhs, keymap.rhs, opts)
+	vim.keymap.set(keymap.mode, keymap.lhs, keymap.rhs, opts)
 end
 
 ---@param client vim.lsp.Client
 ---@param bufnr number
 M.setup = function(client, bufnr)
 	local keymaps = util.map_array_to_table(config.mapping.get_namespaced_keymaps("nvim-lspconfig-common"), "lhs")
-  ---@type config.mapping.ExtraKeymapsetOpts
-  local extra_opts = { buffer = bufnr }
-
-  -- check for hover support
+	---@type config.mapping.ExtraKeymapsetOpts
+	local extra_opts = { buffer = bufnr }
 	if client:supports_method(methods.textDocument_hover) then
     set_keymap(keymaps["K"], extra_opts)
   end
