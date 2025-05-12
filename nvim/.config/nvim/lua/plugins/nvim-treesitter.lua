0322ea15a359d0d4be4f25ba1b7a91c097a5464d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-05-11 21:55:06 -0600

    add: add events to lazy-load nvim-tresitter

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
 	opts = require("plugins.opts.nvim-treesitter"),
 	config = function(lazyplugin, opts)
 		require("nvim-treesitter.configs").setup(opts)
