72aba27c8255e3851c11b0639c62d185b9d6dd0b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-05-16 16:59:04 -0600

    add: note about ecma text object extension
    The nodes we capture with the @property groups are already defined in
    the queries/ecma/textobjects.scm file, captured by @assignment groups.

--- nvim/.config/nvim/after/queries/ecma/textobjects.scm
+++ nvim/.config/nvim/after/queries/ecma/textobjects.scm
@@ -1,5 +1,6 @@
 ; extends
 
+; note: already exists in ~/.local/share/nvim/lazy/nvim-treesitter-textobjects/queries/ecma/textobjects.scm with @assignment capture groups instead of @property
 (object
   (pair
     key: (_) @property.lhs
