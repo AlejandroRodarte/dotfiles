189018799a2e8f1db04b8ee8ec19bf4fb4fffd0a
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-05-16 16:36:41 -0600

    add: @property capture groups for class fields/properties

--- nvim/.config/nvim/after/queries/typescript/textobjects.scm
+++ nvim/.config/nvim/after/queries/typescript/textobjects.scm
@@ -5,3 +5,9 @@
     name: (_) @property.lhs
     type: (type_annotation
       (_) @property.inner @property.rhs)) @property.outer)
+
+(class_body
+  (public_field_definition
+    (accessibility_modifier)?
+    name: (property_identifier) @property.lhs
+    value: (_) @property.inner @property.rhs) @property.outer)
