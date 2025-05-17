93b9d338290d0882021f2f4bf8684c1e6ce13c69
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-05-17 01:51:43 -0600

    add: svelte language server

--- nvim/.config/nvim/lua/plugins/opts/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/opts/mason-lspconfig.lua
@@ -1,3 +1,12 @@
 return {
-	ensure_installed = { "cssls", "emmet_language_server", "gopls", "java_language_server", "lua_ls", "tailwindcss", "ts_ls" },
+	ensure_installed = {
+		"cssls",
+		"emmet_language_server",
+		"gopls",
+		"java_language_server",
+		"lua_ls",
+		"svelte",
+		"tailwindcss",
+		"ts_ls",
+	},
 }
