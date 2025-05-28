8080b9411bfd221bd1a0618fa3a08e5e7e75d9f2
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-05-27 18:37:17 -0600

    add: initial qutebrowser config

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,6 @@
+c.tabs.title.format = "{audio}{current_title}"
+
+c.url.searchengines = {
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "!yt": "https://youtube.com/results?search_query={}",
+}
