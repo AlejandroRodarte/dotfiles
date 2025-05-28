cdaf30c6a2eac4d108cee6b8df838a43112f4563
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-05-27 19:15:56 -0600

    add: catppuccin mocha theme for qutebrowser
    This will only work if the python catppuccin module is installed in
    ~/.config/qutebrowser/catppuccin.

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,3 +1,5 @@
+import catppuccin
+
 c.tabs.title.format = "{audio}{current_title}"
 
 c.url.searchengines = {
@@ -10,3 +12,7 @@ c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.algorithm = "lightness-cielab"
 c.colors.webpage.darkmode.policy.images = "never"
 config.set("colors.webpage.darkmode.enabled", False, "file://*")
+
+# enable catppuccin (manually installed in ~/.config/qutebrowser/catppuccin)
+# [these instructions were followed](https://github.com/catppuccin/qutebrowser/tree/main#manual-config)
+catppuccin.setup(c, "mocha", False)
