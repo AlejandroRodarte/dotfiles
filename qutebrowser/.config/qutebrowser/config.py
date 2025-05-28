ed7a4e5a6164c4930be6819fb63b0317b3dfe355
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-05-27 18:52:53 -0600

    add: qutebrowser dark mode

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -4,3 +4,9 @@ c.url.searchengines = {
     "DEFAULT": "https://duckduckgo.com/?q={}",
     "!yt": "https://youtube.com/results?search_query={}",
 }
+
+# dark mode config
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.algorithm = "lightness-cielab"
+c.colors.webpage.darkmode.policy.images = "never"
+config.set("colors.webpage.darkmode.enabled", False, "file://*")
