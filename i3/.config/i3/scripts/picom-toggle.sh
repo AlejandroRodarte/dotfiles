45bd1b2cdd8e0e39120b1588c01b7e4587f04773
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-06-24 15:07:55 -0600

    add: notifications to picom toggle script

--- i3/.config/i3/scripts/picom-toggle.sh
+++ i3/.config/i3/scripts/picom-toggle.sh
@@ -3,6 +3,12 @@
 if pgrep -x "picom" > /dev/null
 then
   killall picom
+  if [ $? -eq 0 ]; then
+    notify-send "Picom disabled" "Picom has been succesfully disabled. You can play games now!"
+  fi
 else
   picom -b --config ~/.config/picom/picom.conf
+  if [ $? -eq 0 ]; then
+    notify-send "Picom enabled" "Picom has been succesfully enabled. Don't use it while playing games!"
+  fi
 fi
