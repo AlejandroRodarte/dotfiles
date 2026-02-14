d2dc2aa9d5caf103631f60c07bf070a78cdbda77
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-02-14 12:11:05 -0600

    move: embed logic we had on lua/plugins/opts/nvim-cmp.lua directly in lua/plugins/nvim-cmp.lua
    This is mainly to get data type information when building the opts
    object for the cmp.setup() function when configuring the plugin.

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,7 +1,49 @@
+local util = require("util");
+local config = require("config");
+
 return {
 	"hrsh7th/nvim-cmp",
 	config = function(lazyplugin, opts)
 		local cmp = require("cmp")
-		cmp.setup(require("plugins.opts.nvim-cmp").build_opts(cmp))
+    cmp.setup({
+			formatting = {
+				format = require("lspkind").cmp_format({
+					mode = "symbol",
+					maxwidth = {
+						menu = 50,
+						abbr = 50,
+					},
+					ellipsis_char = "...",
+					show_labelDetails = true,
+					before = require("tailwind-tools.cmp").lspkind_format,
+				}),
+			},
+			snippet = {
+				-- let luasnip handle snippet expansion
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert(
+				util.map_array_to_table(config.mapping.get_namespaced_keymaps("nvim-cmp"), function(keymap)
+					return keymap.lhs
+				end, util.keymap_to_rhs)
+			),
+			sources = cmp.config.sources({
+				{
+					name = "lazydev",
+					group_index = 0, -- set group index to 0 to skip loading luals completions
+				},
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "nvim_lsp_signature_help" },
+			}, {
+				{ name = "buffer" },
+			}),
+		})
 	end,
 }
